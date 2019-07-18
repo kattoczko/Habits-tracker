723b81bcaa7773757301bb579d327cb6f67d2c69
Katarzyna <kasia.oczkowska@gmail.com>
2019-07-18 08:00:23 +0800

    App component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HabitsTable from "../HabitsTable/HabitsTable";
+
+export default function App() {
+  return (
+    <div>
+      <HabitsTable />
+    </div>
+  );
+}
